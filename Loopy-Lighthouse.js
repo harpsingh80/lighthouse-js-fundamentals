4d9c6fc592f521e724db0684570b2e1769f06bec
Harry Parmar <harryparmar@Harrys-MacBook-Pro.local>
2022-01-20 15:20:54 -0800

    Loopy Lighthouse

--- Loopy-Lighthouse.js
+++ Loopy-Lighthouse.js
@@ -1,11 +1,11 @@
-for(var i = 100; i <= 200; i++){
-	if(i % 3 === 0 && i % 4 === 0){
-		console.log("LoopyLighthouse");
-	}else if(i % 3 === 0){
-		console.log("Loopy");
-	}else if(i % 4 === 0){
-		console.log("Lighthouse");
-	}else{
-		console.log(i);
-	}
-}
+for (var x = 100; x <= 200; x++) {
+  if (x % 3 === 0 && x % 4 === 0) {
+    console.log("LoopyLighthouse");
+  } else if (x % 3 === 0) {
+    console.log("Loopy");
+  } else if (x % 4 === 0) {
+    console.log("Lighthouse");
+  } else {
+    console.log(x);
+  }
+} 
