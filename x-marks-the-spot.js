d13bb9a323894c58245dbaf68b2fce75e61d42ec
Harry Parmar <harryparmar@Harrys-MacBook-Pro.local>
2022-01-22 13:26:20 -0800

    x-marks-the-spot

---
+++ x-marks-the-spot.js
@@ -0,0 +1,20 @@
+const finalPosition = function (moves) {
+  let finalPosition = [0, 0]; //[(east,west),(north,south)]
+  for (const move of moves) {
+    switch (move) {
+    case "north":
+      finalPosition[1] += 1;
+      break;
+    case "east":
+      finalPosition[0] += 1;
+      break;
+    case "south":
+      finalPosition[1] -= 1;
+      break;
+    case "west":
+      finalPosition[0] -= 1;
+      break;
+    }
+  }
+  return finalPosition;
+};
